76975c8d9957b22fb00899ce2a7350981576d032
Oliwier Stępnia <oliwierstepniak@gmail.com>
2023-04-28 22:30:46 +0200

    Create basics for game class.

--- Game.cpp
+++ Game.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Game.h"
+
+void Game::update() {
+
+}
+
+void Game::render() {
+
+}
+
+void Game::initVariables() {
+
+}
+
+void Game::initWindow() {
+    this->videoMode.height = 600;
+    this->videoMode.width = 800;
+
+    this->pWindow = new sf::RenderWindow(this->videoMode, "Game 1", sf::Style::Titlebar | sf::Style::Close);
+
+    this->pWindow->setFramerateLimit(60);
+}
+
+Game::Game() {
+    this->initVariables();
+    this->initWindow();
+}
+
+Game::~Game() {
+    delete this->pWindow;
+}
+
+const bool Game::isRunning() const {
+    return this->pWindow->isOpen();
+}
--- Game.h
+++ Game.h
@@ -4,10 +4,38 @@
 
 #ifndef PROJEKT_PODBIJANIE_KAMPUSU_MICROSOFTU_GAME_H
 #define PROJEKT_PODBIJANIE_KAMPUSU_MICROSOFTU_GAME_H
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 
-class Game {
 
+class Game{
+private:
+    //Variables
+    //Window
+    sf::RenderWindow* pWindow;
+    sf::VideoMode videoMode;
+    sf::Event event;
+
+    //functions
+    void initVariables();
+    void initWindow();
+
+public:
+    Game();
+    virtual ~Game();
+
+    //Variables
+    bool isTuxSurvive;
+
+    //functions
+    const bool isRunning() const;
+
+    void update();
+    void render();
 };
 
 
