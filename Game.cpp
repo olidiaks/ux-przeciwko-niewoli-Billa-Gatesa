d9b0084185b206ac02ea77b584dcc178608a5753
Oliwier Stępnia <oliwierstepniak@gmail.com>
2023-04-28 22:28:33 +0200

    Add some coments.

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by olidiaks on 28.04.23.
+//
+
+#include "Game.h"
--- main.cpp
+++ main.cpp
@@ -2,21 +2,24 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <ctime>
+#include "Game.h"
 
 int main() {
-//
-//            << "W jakim języku chcesz grać> pl (polskim) lub en (angielskim)\nChose language PL (polish) or EN (english)\n";
-//    std::string language;
-//    std::cin >> language;
+    //Init srand
+    srand(static_cast<unsigned>(time(NULL)));
+
+    //Get player name.
     std::cout << "Podaj swoje imię.\n";
     std::string name;
     std::cin>>name;
 
+    //get file with quests.
     std::ifstream file("questionspl.csv");
-//    script.open("questions" + language + ".csv");
 
     bool isWordSavedForBillGates = true;
 
+    //next game sitting
     std::string row;
     while (getline(file, row)) {
         std::string questionId;
@@ -24,6 +27,7 @@ int main() {
         std::string answers[4];
         std::string *pAnswers = answers;
 
+        //showing game questions and answers.
         std::stringstream stringStream(row);
         std::getline(stringStream, questionId, ';');
         std::getline(stringStream, question, ';');
@@ -32,30 +36,42 @@ int main() {
         std::getline(stringStream, *(pAnswers + 2), ';');
         std::getline(stringStream, *(pAnswers + 3), ';');
         std::getline(stringStream, correctAnswer, ';');
-
         std::cout << questionId << ". " << question << "\n1) " << *answers << "\n2) " << *(answers + 1) << "\n3) "
                   << *(pAnswers + 2) << "\n4)" << *(pAnswers + 3);
 
+        //catch answer
         int answer;
         std::cin >> answer;
         system("cls");
         system("clear");
+        //Tux survive or die.
         if (*(answers + answer - 1) == correctAnswer) {
             std::cout
                     << " udał ci się poprawnie odpowiedzieć nadal żyjesz. Do zobaczenia w kolejnej rundzie\n\n\n\n";
         } else {
-            isWordSavedForBillGates = false;
-            std::cout
-                    << "Jak by to delikatnie powiedzieć?\nO już wiem.\nTux przestał przypominać siebie!\nDoprowadziłeś do jego śmierci.\nRuch oporu przeciwko Bilowi Gatesowi i jego planom na zniewolenie świata. Upadł, ponieważ zabiłeś jego przywódcę\nTeraz nik go już nie powstrzyma wszyscy skończymy jako jego niewolnicy.";
+            Game game;
+            while (game.isRunning()){
+
+                //Update
+                game.update();
+
+                //Render
+                game.render();
+            }
+            isWordSavedForBillGates = game.isTuxSurvive();
             break;
         }
     }
     file.close();
 
+    //win
     if (isWordSavedForBillGates){
         system("cls");
         system("clear");
         std::cout << "Brawo " << name << ", udało ci się ocalić świat przed złym wrednym Billem Gatesem.\n";
+    } else{
+        std::cout
+                << "Jak by to delikatnie powiedzieć?\nO już wiem.\nTux przestał przypominać siebie!\nDoprowadziłeś do jego śmierci.\nRuch oporu przeciwko Bilowi Gatesowi i jego planom na zniewolenie świata. Upadł, ponieważ zabiłeś jego przywódcę\nTeraz nik go już nie powstrzyma wszyscy skończymy jako jego niewolnicy.";
     }
     return 0;
 }
